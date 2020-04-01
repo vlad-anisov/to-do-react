108423e1e19a84a7e1ce2c1f75a3d59a70e6f085
vlad-anisov <anisoffgo@gmail.com>
2020-04-01 21:11:47 +0300

    fix

--- todo/settings.py
+++ todo/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,6 +130,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 
-CORS_ORIGIN_WHITELIST  =  [ 
-    "http://localhost:3000", 
-]
+CORS_ORIGIN_ALLOW_ALL = True
